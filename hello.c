074de9d7c29eb4418248937e2839554985cd22a5
robin <heartinharbin@gmail.com>
2018-11-01 20:44:54 +0800

    first commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+ ///
+ /// @file    hello.c
+ /// @author  lemon(haohb13@gmail.com)
+ /// @date    2018-11-01 20:43:39
+ ///
+ 
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main()
+{
+	printf("hello world!\n");
+}
+
+
